7e7c094945a3a8f2f54ea396ee718587dd8e5ea7
muhrafifrbn <afifrabbani04@gmail.com>
2024-10-08 20:20:31 +0700

    Perbaikan kode dan tampilan

---
+++ src/assets/gambar-mobil.js
@@ -0,0 +1,9 @@
+import carGallardo from "../assets/img/Gambar-mobil/car-1.jpg";
+import carScirocco from "../assets/img/Gambar-mobil/car-2.jpg";
+import carBmw435i from "../assets/img/Gambar-mobil/car-3.jpg";
+import carMercyG63 from "../assets/img/Gambar-mobil/car-4.jpg";
+import carCarrera from "../assets/img/Gambar-mobil/car-5.jpg";
+import carMercySls from "../assets/img/Gambar-mobil/car-7.jpg";
+import carBmw640i from "../assets/img/Gambar-mobil/car-8.jpg";
+
+export { carBmw435i, carGallardo, carScirocco, carMercyG63, carCarrera, carMercySls, carBmw640i };
