b11dccbc1a5efe8ac2bf2f05b91789f6eb439d8a
muhrafifrbn <afifrabbani04@gmail.com>
2024-10-12 13:58:10 +0700

    Menambahkan gambar dan mengubah ukuran gambar

--- src/assets/gambar-mobil.js
+++ src/assets/gambar-mobil.js
@@ -1,9 +1,14 @@
-import carGallardo from "../assets/img/Gambar-mobil/car-1.jpg";
-import carScirocco from "../assets/img/Gambar-mobil/car-2.jpg";
-import carBmw435i from "../assets/img/Gambar-mobil/car-3.jpg";
-import carMercyG63 from "../assets/img/Gambar-mobil/car-4.jpg";
-import carCarrera from "../assets/img/Gambar-mobil/car-5.jpg";
-import carMercySls from "../assets/img/Gambar-mobil/car-7.jpg";
-import carBmw640i from "../assets/img/Gambar-mobil/car-8.jpg";
+import carGallardo from "./img/Gambar-mobil/car-1.jpg";
+import carScirocco from "./img/Gambar-mobil/car-2.jpg";
+import carBmw435i from "./img/Gambar-mobil/car-3.jpg";
+import carMercyG63 from "./img/Gambar-mobil/car-4.jpg";
+import carCarrera from "./img/Gambar-mobil/car-5.jpg";
+import carMercySls from "./img/Gambar-mobil/car-7.jpg";
+import carBmw640i from "./img/Gambar-mobil/car-8.jpg";
+import carBmw430G from "./img/Gambar-mobil/car-9.jpg";
+import carNissanGtr from "./img/Gambar-mobil/car-10.jpg";
+import carBmwWagon from "./img/Gambar-mobil/car-11.jpg";
+import carMercyS63 from "./img/Gambar-mobil/car-12.jpg";
+import carBmwM6 from "./img/Gambar-mobil/car-13.jpg";
 
-export { carBmw435i, carGallardo, carScirocco, carMercyG63, carCarrera, carMercySls, carBmw640i };
+export { carBmwM6, carMercyS63, carBmwWagon, carNissanGtr, carBmw430G, carBmw435i, carGallardo, carScirocco, carMercyG63, carCarrera, carMercySls, carBmw640i };
